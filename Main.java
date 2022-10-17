d43165d4f6a8fd907759f6a1019c476a437ff716
RenderNNNN <111516134+RenderNNNN@users.noreply.github.com>
2022-10-17 15:06:47 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,2 +1,29 @@
-public class Main {
+public class Main{
+    public static CircularlyLinkedList board = new CircularlyLinkedList();
+    //public static Property
+    public static void main(String[] args){
+
+        Player a = new Player("Yo", 9);
+        Player b = new Player("Sup", 9);
+        Player c = new Player("Yo", 9);
+        if(a.equals(c)){ System.out.println("TEST"); }
+
+        /*Property[] purple = Property.properties[0];
+        purple[0].owner = a;
+        purple[1].owner = a;
+        a.balance = 9999;
+        purple[1].canBuy(0, 0);*/
+
+        //System.out.println(purple[1].canBuy(0, 0));
+        /*list.insertRight(1);
+        list.insertRight(2);
+        list.insertRight(3);*/
+
+        /*board.insert(1, true);
+        board.insert(2, true);
+        board.insert(3, true);
+
+        System.out.println(board.toString(false));*/
+        //list.examine();
+    }
 }
