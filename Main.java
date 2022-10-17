e0acadb2c47ed1c85870fd746916fb9c88766d3d
nathanvits <nfvits@icloud.com>
2022-10-17 15:11:39 -0500

    Testing first commit

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 public class Main{
     public static CircularlyLinkedList board = new CircularlyLinkedList();
     //public static Property
+    //a
     public static void main(String[] args){
 
         Player a = new Player("Yo", 9);
